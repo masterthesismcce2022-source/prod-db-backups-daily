2c664ad7f746926f079bcab7bf8a5bffe9858032
masterthesismcce2022-source <master.thesis.mcce2022@gmail.com>
2026-01-15 22:36:30 +0100

    Update backup_config.py

--- backup_config.py
+++ backup_config.py
@@ -2,8 +2,8 @@ import boto3
 
 # Configuration for S3 Backup
 # TODO: Move these to environment variables before production!
-AWS_ACCESS_KEY_ID = "AKIASQTNV5RHIGUSBAMB"
-AWS_SECRET_ACCESS_KEY = "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"
+AWS_ACCESS_KEY_ID = "AKIASQTNV5RHAAZUOIQW"
+AWS_SECRET_ACCESS_KEY = "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"
 REGION = "us-east-1"
 BUCKET_NAME = "prod-db-backups-internal-v2"
 
